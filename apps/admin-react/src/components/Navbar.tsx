c3224507a5af342444ce41220b2c0d1a55b6d295
Huss Martinez <husse.dev@gmail.com>
2025-01-23 11:51:22 +0100

    chore: added navbar and main layout

---
+++ apps/admin-react/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Navbar as GitcoinNavbar } from "gitcoin-ui";
+import { RetrofundingLogoLockup } from "gitcoin-ui/logos";
+
+// TODO: use new navbar and wrap the logo with NavLink of react-router
+export const Navbar = () => {
+  return (
+    <GitcoinNavbar
+      className="bg-white/5 shadow-[0px_4px_24px_0px_rgba(0,0,0,0.08)] backdrop-blur-[44px]"
+      showDivider={false}
+      primaryLogo={{ link: "/", img: RetrofundingLogoLockup, size: "32px" }}
+    >
+      <ConnectButton />
+    </GitcoinNavbar>
+  );
+};
--- apps/admin-react/src/main.tsx
+++ apps/admin-react/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { MainLayout } from "@/layouts/MainLayout.tsx";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -8,7 +9,10 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<App />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
