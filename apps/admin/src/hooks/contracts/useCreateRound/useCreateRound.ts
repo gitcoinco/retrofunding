f2ccd10a0768ed7f9f8a936b4437718d3f02caa6
0xKurt <kurt@merbeth.io>
2025-01-29 16:46:09 +0100

    add contract interaction wrapper hook

--- apps/admin/src/hooks/contracts/useCreateRound/useCreateRound.ts
+++ apps/admin/src/hooks/contracts/useCreateRound/useCreateRound.ts
@@ -1,202 +1,102 @@
-import { useState, useEffect, useMemo } from "react";
-import { Allo, TransactionData } from "@allo-team/allo-v2-sdk";
-import { EasyRetroFundingStrategy } from "@allo-team/allo-v2-sdk";
+import { Allo, EasyRetroFundingStrategy, Registry } from "@allo-team/allo-v2-sdk";
 import { InitializeData } from "@allo-team/allo-v2-sdk/dist/strategies/EasyRetroFunding/types";
 import { getChainById } from "@gitcoin/gitcoin-chain-data";
-import { ProgressStatus } from "@gitcoin/ui/types";
 import { useMutation } from "@tanstack/react-query";
-import moment from "moment-timezone";
-import { createPublicClient, Hex, http, zeroAddress } from "viem";
-import { useWalletClient } from "wagmi";
+import moment from "moment";
+import { Address, Hex, zeroAddress } from "viem";
+import { useAccount } from "wagmi";
+import { getCreateRoundProgressSteps } from "@/hooks";
 import { uploadData } from "@/services/ipfs/upload";
-import { targetNetworks } from "@/services/web3/chains";
-import { AnyJson, RoundSetupFormData } from "@/types";
 import { mapFormDataToRoundMetadata } from "@/utils/transformRoundMetadata";
 import { UINT64_MAX } from "@/utils/utils";
-import { waitUntilIndexerSynced, getCreateRoundProgressSteps } from "../utils";
+import { useContractInteraction } from "../useContractInteraction/useContractInteraction";
+import { CreateRoundParams } from "./useCreateRound.old";
 
-export type CreateRoundParams = {
-  data: RoundSetupFormData;
+export type CreateProgramParams = {
+  chainId: number;
+  programName: string;
+  members: Address[];
 };
 
 export const useCreateRound = () => {
-  const [uploadMetadataStatus, setUploadMetadataStatus] = useState<ProgressStatus>(
-    ProgressStatus.NOT_STARTED,
-  );
-  const [contractUpdatingStatus, setContractUpdatingStatus] = useState<ProgressStatus>(
-    ProgressStatus.NOT_STARTED,
-  );
-  const [indexingStatus, setIndexingStatus] = useState<ProgressStatus>(ProgressStatus.NOT_STARTED);
-  const [finishingStatus, setFinishingStatus] = useState<ProgressStatus>(
-    ProgressStatus.NOT_STARTED,
-  );
-
-  const { data: walletClient } = useWalletClient();
+  const { steps, contractInteractionMutation } = useContractInteraction();
 
   const createRoundMutation = useMutation({
     mutationFn: async ({ data }: CreateRoundParams) => {
-      if (!walletClient) {
-        throw new Error("WalletClient is undefined");
-      }
-
       const chainId = data.program.chainId;
-
-      if (walletClient.chain.id !== chainId) {
-        await walletClient.switchChain({
-          id: chainId,
-        });
-      }
-
-      const chain = targetNetworks.find((chain) => chain.id === chainId);
-      const account = walletClient.account;
-
-      if (!account) {
-        throw new Error("WalletClient account is undefined");
-      }
-
-      setUploadMetadataStatus(ProgressStatus.IN_PROGRESS);
-
-      const publicClient = createPublicClient({
-        chain,
-        transport: http(),
-      });
-
       const roundImageIpfs = await uploadData(data.coverImage);
 
       if (roundImageIpfs.type === "error") {
-        setUploadMetadataStatus(ProgressStatus.IS_ERROR);
-        return { status: "error", error: new Error("Failed to upload round image to IPFS") };
+        throw new Error("Failed to upload round image to IPFS");
       }
 
       const mappedMetadata = mapFormDataToRoundMetadata(data, roundImageIpfs.value);
 
       mappedMetadata.round.retroFundingConfig.coverImage = roundImageIpfs.value;
 
-      const roundMetadataIpfs = await uploadData(mappedMetadata as unknown as AnyJson);
+      return contractInteractionMutation.mutateAsync({
+        chainId,
+        metadata: mappedMetadata,
+        transactionData: async (metadataCid?: string) => {
+          const allo = new Allo({
+            chain: chainId,
+          });
 
-      if (roundMetadataIpfs.type === "error") {
-        setUploadMetadataStatus(ProgressStatus.IS_ERROR);
-        return { status: "error", error: new Error("Failed to upload round metadata to IPFS") };
-      }
+          const strategyAddress = getChainById(chainId).contracts.retroFunding;
 
-      setUploadMetadataStatus(ProgressStatus.IS_SUCCESS);
-      const roundMetadataIpfsCid = roundMetadataIpfs.value;
+          if (!strategyAddress) {
+            throw new Error("Strategy address is undefined for chainId: " + chainId);
+          }
 
-      const allo = new Allo({
-        chain: chainId,
+          const retroFunding = new EasyRetroFundingStrategy({
+            chain: chainId,
+            address: strategyAddress,
+          });
+
+          const dates = data.roundDates;
+          const timezone = data.timezone;
+
+          const registrationStartTime = BigInt(
+            moment.tz(dates.applications.start, timezone).unix(),
+          );
+          const registrationEndTime = BigInt(moment.tz(dates.applications.end, timezone).unix());
+          const poolStartTime = BigInt(moment.tz(dates.round.start, timezone).unix());
+          const poolEndTime = dates.round.noEndDate
+            ? UINT64_MAX
+            : BigInt(moment.tz(dates.round.end, timezone).unix());
+
+          const initializeData: InitializeData = {
+            useRegistryAnchor: true,
+            metadataRequired: true,
+            registrationStartTime,
+            registrationEndTime,
+            poolStartTime,
+            poolEndTime,
+          };
+
+          const initData = await retroFunding.getInitializeData(initializeData);
+
+          ///
+          if (!metadataCid) throw new Error("Metadata CID is required");
+          const nonce = BigInt(Math.floor(Math.random() * 1000000000));
+
+          return allo.createPool({
+            profileId: data.program.programId as Hex,
+            strategy: strategyAddress,
+            initStrategyData: initData,
+            token: zeroAddress, // TODO: payout token should be an address and no name
+            amount: BigInt(0),
+            metadata: {
+              protocol: 1n,
+              pointer: metadataCid,
+            },
+            managers: [], // TODO: do we need managers in our form?
+          });
+        },
+        getProgressSteps: getCreateRoundProgressSteps,
       });
-
-      const strategyAddress = getChainById(chainId).contracts.retroFunding;
-
-      if (!strategyAddress) {
-        throw new Error("Strategy address is undefined for chainId: " + chainId);
-      }
-
-      const retroFunding = new EasyRetroFundingStrategy({
-        chain: chainId,
-        address: strategyAddress,
-      });
-
-      const dates = data.roundDates;
-      const timezone = data.timezone;
-
-      const registrationStartTime = BigInt(moment.tz(dates.applications.start, timezone).unix());
-      const registrationEndTime = BigInt(moment.tz(dates.applications.end, timezone).unix());
-      const poolStartTime = BigInt(moment.tz(dates.round.start, timezone).unix());
-      const poolEndTime = dates.round.noEndDate
-        ? UINT64_MAX
-        : BigInt(moment.tz(dates.round.end, timezone).unix());
-
-      const initializeData: InitializeData = {
-        useRegistryAnchor: true,
-        metadataRequired: true,
-        registrationStartTime,
-        registrationEndTime,
-        poolStartTime,
-        poolEndTime,
-      };
-
-      const initData = await retroFunding.getInitializeData(initializeData);
-      let txData: TransactionData;
-
-      try {
-        txData = allo.createPool({
-          profileId: data.program.programId as Hex,
-          strategy: strategyAddress,
-          initStrategyData: initData,
-          token: zeroAddress, // TODO: payout token should be an address and no name
-          amount: BigInt(0),
-          metadata: {
-            protocol: 1n,
-            pointer: roundMetadataIpfsCid,
-          },
-          managers: [], // TODO: do we need managers in our form?
-        });
-      } catch (e) {
-        setContractUpdatingStatus(ProgressStatus.IS_ERROR);
-        throw new Error("Failed to update application status");
-      }
-
-      setContractUpdatingStatus(ProgressStatus.IN_PROGRESS);
-
-      let txHash;
-      try {
-        txHash = await walletClient.sendTransaction({
-          account: account,
-          to: txData.to,
-          data: txData.data,
-          chain: chain,
-        });
-      } catch (e) {
-        setContractUpdatingStatus(ProgressStatus.IS_ERROR);
-        throw new Error("Failed to update application status");
-      }
-
-      const receipt = await publicClient.waitForTransactionReceipt({ hash: txHash });
-
-      if (!receipt.status) {
-        setContractUpdatingStatus(ProgressStatus.IS_ERROR);
-        throw new Error("Failed to update application status");
-      }
-
-      setContractUpdatingStatus(ProgressStatus.IS_SUCCESS);
-      setIndexingStatus(ProgressStatus.IN_PROGRESS);
-
-      try {
-        await waitUntilIndexerSynced({
-          chainId,
-          blockNumber: receipt.blockNumber,
-        });
-      } catch (e) {
-        setIndexingStatus(ProgressStatus.IS_ERROR);
-      }
-
-      setIndexingStatus(ProgressStatus.IS_SUCCESS);
-      setFinishingStatus(ProgressStatus.IN_PROGRESS);
-      setFinishingStatus(ProgressStatus.IS_SUCCESS);
     },
   });
 
-  useEffect(() => {
-    if (createRoundMutation.isSuccess) {
-      setContractUpdatingStatus(ProgressStatus.NOT_STARTED);
-      setIndexingStatus(ProgressStatus.NOT_STARTED);
-      setFinishingStatus(ProgressStatus.NOT_STARTED);
-      createRoundMutation.reset();
-    }
-  }, [createRoundMutation]);
-
-  const steps = useMemo(() => {
-    return getCreateRoundProgressSteps({
-      uploadMetadataStatus,
-      contractUpdatingStatus,
-      indexingStatus,
-      finishingStatus,
-    });
-  }, [uploadMetadataStatus, contractUpdatingStatus, indexingStatus, finishingStatus]);
-
-  return {
-    steps,
-    createRoundMutation,
-  };
+  return { steps, createRoundMutation };
 };
--- apps/admin/src/services/backend/dataLayer.ts
+++ apps/admin/src/services/backend/dataLayer.ts
@@ -15,7 +15,6 @@ export const getMetrics = async (): Promise<Metric[]> => {
 export const getPool = async (alloPoolId: string, chainId: number): Promise<Pool> => {
   try {
     const response = await executeQuery(getPoolQuery, { alloPoolId, chainId });
-    console.log("===<", response);
     const pool = response.pools[0];
     pool.metricIdentifiers = pool.metricIdentifiers.split(",");
     pool.distributionData = JSON.parse(pool.distributionData) as PoolDistribution;
