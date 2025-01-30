454436c26d3cb16e4c07ff1bcc5ade1a0040453c
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-01-30 19:48:12 +0530

    wire in update eligibility

--- apps/admin/src/pages/ManagePool/tabs/TabVoter.tsx
+++ apps/admin/src/pages/ManagePool/tabs/TabVoter.tsx
@@ -1,7 +1,9 @@
 import { Form } from "@gitcoin/ui/client";
+import { toast } from "@gitcoin/ui/hooks/useToast";
 import { FormField } from "@gitcoin/ui/types";
 import { MessagePage } from "@/components/Message";
 import { useGetPool } from "@/hooks/backend/useGetPool";
+import { updatePoolEligibility } from "@/services/backend/api";
 
 export const TabVoter = ({ chainId, poolId }: { chainId: number; poolId: string }) => {
   const { data: pool, isLoading } = useGetPool(poolId, chainId);
@@ -40,12 +42,25 @@ export const TabVoter = ({ chainId, poolId }: { chainId: number; poolId: string 
     },
   };
 
-  console.log("===<", pool?.eligibilityCriteria.data.voters);
-  console.log(pool);
-
   if (isLoading) {
     return <MessagePage title="Loading..." message="Loading..." />;
   }
 
-  return <Form step={voterStep} onSubmit={async (values: any) => console.log(values)} />;
+  const handleSubmit = async (values: any) => {
+    await updatePoolEligibility({
+      alloPoolId: poolId,
+      chainId: chainId,
+      eligibilityType: "linear",
+      data: {
+        voters: values.voterAllowlist,
+      },
+    });
+
+    toast({
+      status: "success",
+      description: "Voter allowlist updated successfully",
+    });
+  };
+
+  return <Form step={voterStep} onSubmit={async (values: any) => await handleSubmit(values)} />;
 };
