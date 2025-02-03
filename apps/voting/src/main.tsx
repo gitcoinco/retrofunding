c0f5f31d2d522bc3d7be578fb52ca54fd8fbe8b6
Huss Martinez <husse.dev@gmail.com>
2025-02-03 18:15:24 +0100

    chore: implemented Voting

--- apps/voting/src/main.tsx
+++ apps/voting/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { MainLayout } from "@/layouts/MainLayout";
-import { Home } from "./pages";
+import { Home, Vote } from "./pages";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -12,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
         <Route element={<MainLayout />}>
           <Route path="*" element={<Home />} />
           <Route path="/:chainId/:roundId" element={<Home />} />
+          <Route path="/:chainId/:roundId/vote" element={<Vote />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ apps/voting/src/pages/Vote/Vote.tsx
@@ -0,0 +1,115 @@
+import { useCallback, useMemo } from "react";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { useToast } from "@gitcoin/ui/hooks/useToast";
+import { MetricsBallot } from "@gitcoin/ui/retrofunding";
+import { isEqual } from "lodash";
+import { Hex } from "viem";
+import { useAccount } from "wagmi";
+import { useGetRoundWithApplications, useVote } from "@/hooks";
+import { useGetMetrics } from "@/hooks/useGetMetrics";
+import { RetroVoteBody, RetroVoteFormData } from "@/types";
+import { Home } from "../Home";
+import { VoteSidebar } from "./components/VoteSidebar";
+
+export const Vote = () => {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentBallot, setCurrentBallot] = useState<
+    { metricIdentifier: string; voteShare: number }[]
+  >([]);
+
+  const { address } = useAccount();
+  const { roundId, chainId: chainIdString } = useParams();
+  const chainId = parseInt(chainIdString as string);
+
+  const { voteMutation } = useVote();
+
+  const { data: round, isLoading: roundIsLoading } = useGetRoundWithApplications({
+    roundId: roundId as string,
+    chainId,
+  });
+
+  const availableMetricsIds = round?.roundMetadata.retroFundingConfig.impactMetrics;
+
+  const { data: metrics, isLoading: metricsIsLoading } = useGetMetrics({
+    identifiers: availableMetricsIds,
+    enabled: !!availableMetricsIds,
+  });
+
+  const availableMetrics = useMemo(
+    () =>
+      metrics?.map((metric) => ({
+        title: metric.title,
+        description: metric.description,
+        metricId: metric.identifier,
+      })) ?? [],
+    [metrics],
+  );
+
+  const handleFormChange = useCallback((formData: RetroVoteFormData) => {
+    const ballot = formData.metrics.map(({ metricId, amount }) => ({
+      metricIdentifier: metricId,
+      voteShare: amount,
+    }));
+    if (!isEqual(ballot, currentBallot)) {
+      setCurrentBallot(ballot);
+    }
+  }, []);
+
+  const handleSubmit = ({ metrics }: RetroVoteFormData) => {
+    if (roundId && chainId) {
+      const alloPoolId = roundId;
+      const signature = "0xdeadbeef";
+      const voter = address as Hex;
+      const ballot = metrics.map(({ metricId, amount }) => ({
+        metricIdentifier: metricId,
+        voteShare: amount,
+      }));
+      const voteBody: RetroVoteBody = {
+        alloPoolId,
+        chainId,
+        voter,
+        signature,
+        ballot,
+      };
+      voteMutation.mutate(voteBody, {
+        onSuccess: () => {
+          toast({
+            status: "success",
+            description: "Vote submitted successfully",
+          });
+        },
+        onError: (error) => {
+          toast({
+            status: "error",
+            description: `Failed to submit vote\n${error}`,
+          });
+        },
+      });
+    } else {
+      toast({
+        status: "error",
+        description: "Round or chainId is not defined",
+      });
+    }
+  };
+
+  if (!address) {
+    return <Home />;
+  }
+
+  return (
+    <div className="flex flex-col lg:flex-row lg:justify-center gap-12 px-20 pt-[52px]">
+      <MetricsBallot
+        className="lg:w-[1000px]"
+        name="metrics"
+        availableMetrics={availableMetrics}
+        maxAllocation={100}
+        onSubmit={handleSubmit}
+        onFormChange={handleFormChange} // TODO: make it optional in core
+      />
+      <VoteSidebar poolId={roundId} chainId={chainId} ballot={currentBallot} />
+    </div>
+  );
+};
