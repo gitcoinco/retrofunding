8f4661ba1ee638f859764345d9487ea57fd6fa37
Huss Martinez <husse.dev@gmail.com>
2025-02-10 22:53:30 +0700

    chore: add Vote logo to voting app navbar (#27)

--- apps/voting/src/components/Navbar.tsx
+++ apps/voting/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Navbar as GitcoinNavbar } from "@gitcoin/ui";
-import { RetrofundingLogoLockup } from "@gitcoin/ui/logos";
+import { RetrofundingLogoLockup, RetrofundingVoteLogoWordmark } from "@gitcoin/ui/logos";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 // TODO: use new navbar and wrap the logo with NavLink of react-router
@@ -9,8 +9,8 @@ export const Navbar = () => {
   return (
     <GitcoinNavbar
       className="bg-white/5 shadow-[0px_4px_24px_0px_rgba(0,0,0,0.08)] backdrop-blur-[44px]"
-      showDivider={false}
       primaryLogo={{ link: "/", img: RetrofundingLogoLockup, size: "32px" }}
+      secondaryLogo={{ link: "/", img: RetrofundingVoteLogoWordmark, size: "32px" }}
     >
       <ConnectButton />
     </GitcoinNavbar>
