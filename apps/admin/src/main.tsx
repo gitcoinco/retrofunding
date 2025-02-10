5de22db4bf3f769bd737c556838d865224259432
Huss Martinez <husse.dev@gmail.com>
2025-02-10 23:27:22 +0700

    chore: moved Web3Providers to main.tsx

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -6,21 +6,24 @@ import App from "./App";
 import { CreateProgram } from "./pages/CreateProgram";
 import { CreateRound } from "./pages/CreateRound";
 import { ManagePool } from "./pages/ManagePool";
+import { Web3Providers } from "./providers";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<App />} />
-          <Route path="/create-program" element={<CreateProgram />} />
-          <Route path="/create-round" element={<CreateRound />} />
-          <Route path="/:chainId/:poolId/manage-round" element={<ManagePool />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <Web3Providers>
+      <HashRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<App />} />
+            <Route path="/create-program" element={<CreateProgram />} />
+            <Route path="/create-round" element={<CreateRound />} />
+            <Route path="/:chainId/:poolId/manage-round" element={<ManagePool />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </Web3Providers>
   </StrictMode>,
 );
--- apps/voting/src/main.tsx
+++ apps/voting/src/main.tsx
@@ -3,20 +3,23 @@ import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router";
 import { MainLayout } from "@/layouts/MainLayout";
 import { Home, Vote } from "./pages";
+import { Web3Providers } from "./providers";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="*" element={<Home />} />
-          <Route path="/:chainId/:roundId" element={<Home />} />
-          <Route path="/:chainId/:roundId/vote" element={<Vote />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <Web3Providers>
+      <HashRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="*" element={<Home />} />
+            <Route path="/:chainId/:roundId" element={<Home />} />
+            <Route path="/:chainId/:roundId/vote" element={<Vote />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </Web3Providers>
   </StrictMode>,
 );
