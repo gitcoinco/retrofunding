afe002a20d709506c31dcbe44e5c5796b6cee34b
Huss Martinez <husse.dev@gmail.com>
2025-02-12 18:56:04 +0700

    feat(admin): implemented ProtectedRoute

--- apps/admin/src/router/Router.tsx
+++ apps/admin/src/router/Router.tsx
@@ -1,18 +1,20 @@
 import { Routes, Route } from "react-router";
-import App from "@/App";
 import { MainLayout } from "@/layouts/MainLayout";
-import { CreateProgram } from "@/pages";
+import { Admin, CreateProgram, Landing } from "@/pages";
 import { CreateRound } from "@/pages/CreateRound";
 import { ManagePool } from "@/pages/ManagePool";
+import { ProtectedRoute } from "./protectedRoutes";
 
 export const Router = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<App />} />
-        <Route path="/create-program" element={<CreateProgram />} />
-        <Route path="/create-round" element={<CreateRound />} />
-        <Route path="/:chainId/:poolId/manage-round" element={<ManagePool />} />
+        <Route element={<ProtectedRoute fallback={Landing} />}>
+          <Route path="/" element={<Admin />} />
+          <Route path="/create-program" element={<CreateProgram />} />
+          <Route path="/create-round" element={<CreateRound />} />
+          <Route path="/:chainId/:poolId/manage-round" element={<ManagePool />} />
+        </Route>
       </Route>
     </Routes>
   );
