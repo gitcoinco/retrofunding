f084bc7c97de3a6f93393335d06eaec90d8974a5
Huss Martinez <husse.dev@gmail.com>
2025-02-18 21:57:37 +0700

    chore: added spinner in landing pages

--- apps/admin/src/pages/Landing.tsx
+++ apps/admin/src/pages/Landing.tsx
@@ -1,9 +1,17 @@
 import { PropsWithChildren } from "react";
+import { Spinner } from "@gitcoin/ui";
 import { LandingPage } from "@gitcoin/ui/retrofunding";
+
+const LoadingSpinner = () => (
+  <div className="flex flex-col items-center justify-center gap-4">
+    <p className="font-ui-sans text-xl font-medium">Loading...</p>
+    <Spinner />
+  </div>
+);
 
 export const Landing = ({
   isLoading,
   children,
 }: PropsWithChildren<{ poolName?: string; poolDescription?: string; isLoading?: boolean }>) => {
-  return <LandingPage type="admin">{isLoading ? <div>Loading...</div> : children}</LandingPage>;
+  return <LandingPage type="admin">{isLoading ? <LoadingSpinner /> : children}</LandingPage>;
 };
