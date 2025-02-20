a4fcb61c6c98f673960c7cd53884d9ec281f6824
nijoe1 <nikolaos@gitcoin.co>
2025-02-20 15:58:03 +0200

    feat: added the function to get the required data from rf-api to calculate the distribution

--- apps/voting/src/services/backend/api.ts
+++ apps/voting/src/services/backend/api.ts
@@ -1,4 +1,10 @@
-import { PredictDistributionBody, Distribution, SyncPoolBody, RetroVoteBody } from "@/types";
+import {
+  PredictDistributionBody,
+  Distribution,
+  SyncPoolBody,
+  RetroVoteBody,
+  PreparedCalculationData,
+} from "@/types";
 
 export async function syncPool(syncPoolBody: SyncPoolBody): Promise<boolean> {
   const url = `${import.meta.env.VITE_RETROFUNDING_ENDPOINT}/api/pool/sync`;
@@ -71,6 +77,38 @@ export async function predictDistribution(
     return response.json();
   } catch (error) {
     console.error("Error predicting distribution:", error);
+    throw error;
+  }
+}
+
+export async function getDataToCalculateDistribution({
+  chainId,
+  alloPoolId,
+}: {
+  chainId?: number;
+  alloPoolId?: string;
+}): Promise<PreparedCalculationData> {
+  const url = `${import.meta.env.VITE_RETROFUNDING_ENDPOINT}/api/vote/getCalculationData`;
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chainId,
+        alloPoolId,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error: ${response.status} - ${errorData.message || "Unknown error"}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error getting data to calculate distribution:", error);
     throw error;
   }
 }
