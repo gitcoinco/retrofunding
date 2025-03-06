a87090eac5e2d822cbac6839076d77adbaed6a1c
0xKurt <kurt@merbeth.io>
2025-03-06 17:30:36 +0100

    stringify-deterministic

--- apps/admin/src/utils/getDeterministicObjectKeccakHash.ts
+++ apps/admin/src/utils/getDeterministicObjectKeccakHash.ts
@@ -1,5 +1,6 @@
+import stringify from "json-stringify-deterministic";
 import { Hex } from "viem";
-import { keccak256, stringify, toHex } from "viem";
+import { keccak256, toHex } from "viem";
 
 export async function getDeterministicObjectKeccakHash<T>(obj: T): Promise<Hex> {
   const deterministicString = stringify(obj);
