77feb8c397951005b26499e5dedca90ebb0c2ca7
nijoe1 <nikolaos@gitcoin.co>
2025-04-11 18:42:56 +0300

    remove hash if exists in the url

--- apps/voting/src/router/Router.tsx
+++ apps/voting/src/router/Router.tsx
@@ -1,9 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router";
 import { MainLayout } from "@/layouts/MainLayout";
 import { Landing, NotFound, Vote, Leaderboard } from "@/pages";
 import { ProtectedVoteRoute } from "./protectedRoutes";
 
+function useRemoveHashRedirect() {
+  useEffect(() => {
+    const { pathname, hash, search } = window.location;
+
+    // If there's a hash, remove it and update the URL
+    if (hash) {
+      const newPath = pathname + search;
+      // Use replaceState to avoid adding to browser history
+      window.history.replaceState({}, "", newPath);
+    }
+  }, []);
+}
+
 export const Router = () => {
+  useRemoveHashRedirect();
   return (
     <Routes>
       <Route element={<MainLayout />}>
